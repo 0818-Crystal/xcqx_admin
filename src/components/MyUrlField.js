d561b39e76ffe203cafd04f934aa82a8ee0dace5
zh Crystal <391051426@qq.com>
2019-11-16 15:59:44 +0800

    url更改

--- src/components/MyUrlField.js
+++ src/components/MyUrlField.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import LaunchIcon from '@material-ui/icons/Launch';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import LaunchIcon from "@material-ui/icons/Launch";
 
 const styles = {
-    link: {
-        textDecoration: 'none',
-    },
-    icon: {
-        width: '0.5em',
-        paddingLeft: 2,
-    },
+  link: {
+    textDecoration: "none"
+  },
+  icon: {
+    width: "0.5em",
+    paddingLeft: 2
+  }
 };
 
-const MyUrlField = ({ record = {}, source, classes }) =>
-    <a href={record[source]} className={classes.link}>
-<<<<<<< HEAD
-        {record[source]}
-=======
-        {record[source].substring(0, 50)}
->>>>>>> essay has been added an imgae property
-        <LaunchIcon className={classes.icon} />
-    </a>;
+const MyUrlField = ({ record = {}, source, classes }) => (
+  <a href={record[source]} className={classes.link}>
+    {record[source].substring(0, 50)}
+    <LaunchIcon className={classes.icon} />
+  </a>
+);
 
 export default withStyles(styles)(MyUrlField);
